0376609a9a12e126055f289114c42ac97b0b1b8f
Cinfii <168988091+Cinfii@users.noreply.github.com>
2024-05-05 18:37:04 +0100

    Add files via upload

---
+++ username_generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Username Generator!")
+part1 = input("Name an adjective that accurately describes how you feel in your life. ")
+part2 = input("Name your favourite thing in the entire world: ")
+
+print(f"Your username could be {part1} {part2}.")
